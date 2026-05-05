d872ae198e59603a7e03591fc8730b034112a15a
ajarmeh-oroub <ajarmehoroub12@gmail.com>
2026-05-05 12:38:39 +0300

    Fix API key configuration

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
-
-function weather(city){
-
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&lat=44.34&lon=10.99&appid=7eabf17185beb048c35f470eeac890e6`)
-    .then(response => response.json())
-    .then(data => {
- 
-        var mine = document.getElementById('weather-results');
-        mine.innerHTML='';
-
-  
-        var mydiv = document.createElement('div');
-
-        mydiv.innerHTML = `<h1>${data.name}</h1>
-                           <p>Temperature: ${data.main.temp}°F</p>
-                           <p>Humidity: ${data.main.humidity}%</p>
-                           <p>Wind Speed: ${data.wind.speed} mph</p>`;
-
-        mine.appendChild(mydiv);
-    })
-    .catch(error => {
-        console.error('Error fetching the weather data:', error);
-    });}
+function weather(city) {
+    const apiKey = CONFIG.WEATHER_API_KEY;
     
-   
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            var mine = document.getElementById('weather-results');
+            mine.innerHTML = '';
 
-
-function search() {
-    var val = document.getElementById('city-input').value; 
-    weather(val); 
+            var mydiv = document.createElement('div');
+            mydiv.innerHTML = `
+                <h1>${data.name}</h1>
+                <p>Temperature: ${data.main.temp}°F</p>
+                <p>Humidity: ${data.main.humidity}%</p>
+                <p>Wind Speed: ${data.wind.speed} mph</p>`;
+            mine.appendChild(mydiv);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
 }
 
+function search() {
+    const val = document.getElementById('city-input').value;
+    weather(val);
+}
 
-weather('amman')
+weather('amman');
